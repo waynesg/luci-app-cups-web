c273eae79cb0cb86afb0a1a77f7718b2af4b6c30
WAYNE <wayne@WAYNE-APPLE-Mac-mini.lan>
2026-08-12 20:07:50 +0800

    feat: embed cups web in LuCI

--- luci-app-cups-web/htdocs/luci-static/resources/view/cups-web.js
+++ luci-app-cups-web/htdocs/luci-static/resources/view/cups-web.js
@@ -11,8 +11,15 @@ return view.extend({
 		var listen = uci.get('cups-web', 'main', 'listen_addr') || '0.0.0.0:8080';
 		var match = listen.match(/:(\d+)$/);
 		var url = 'http://' + window.location.hostname + ':' + (match ? match[1] : '8080') + '/';
-		window.location.replace(url);
-		return E('div', { 'class': 'spinning' });
+		return E('div', { 'class': 'cbi-map cups-web' }, [
+			E('iframe', {
+				class: 'cups-web-frame',
+				src: url,
+				title: _('Web Printing'),
+				loading: 'eager',
+				style: 'border: 0; display: block; height: calc(100vh - 180px); min-height: 720px; width: 100%;'
+			})
+		]);
 	},
 
 	handleSaveApply: null,
